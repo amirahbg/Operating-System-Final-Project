3784b15c24382f6c3cf8a44524d9c08bd029de9c
amiiir <amirahbg@gmail.com>
2017-07-08 17:43:57 +0430

    update ConstantValue class

--- src/ConstantValue.java
+++ src/ConstantValue.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
 
-public class ConstantValue {
-    public static final int MUTEX_LOCK = 1;
-    public static int N_RESOURCES = 10;
-    public static int N_CLIENTS = 3;
-    public static int NOTIFY_TIME_ELAPSE = 10000;
+class ConstantValue {
+    static final int MUTEX_LOCK = 1;
+    static int N_RESOURCES = 10;
+    static int N_CLIENTS = 3;
+    static int NOTIFY_TIME_ELAPSE = 10000;
+    static int DEADLOCK_MODE = 1;
+    static int PORT_NUMBER = 9898;
+    static final String CONF_PATH = "./src/conf.txt";
+
+    static void init(ArrayList<Integer> setting) {
+        N_CLIENTS = setting.get(0);
+        N_RESOURCES = setting.get(1);
+        DEADLOCK_MODE = setting.get(2);
+        PORT_NUMBER = setting.get(3);
+    }
 }
